af08edb92173823de6eeeb172f30c898c104b820
ALKALiKong <alikongalk@gmail.com>
2020-09-30 20:26:33 +0800

    Use Android.bp for prebuilt

--- Android.mk
+++ Android.mk
@@ -1,17 +1,1 @@
 LOCAL_PATH := $(call my-dir)
-
-include $(call all-subdir-makefiles, $(LOCAL_PATH))
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := via
-LOCAL_MODULE_TAGS := optional
-LOCAL_SRC_FILES := app/via/$(LOCAL_MODULE).apk
-LOCAL_MODULE_CLASS := APPS
-LOCAL_PRIVILEGED_MODULE := true
-LOCAL_CERTIFICATE := PRESIGNED
-LOCAL_DEX_PREOPT := false
-TARGET_OUT_DATA_APPS_PRIVILEGED := $(TARGET_OUT_DATA)/priv-app
-include $(BUILD_PREBUILT)
-
-PRODUCT_PACKAGES += \
-	via
